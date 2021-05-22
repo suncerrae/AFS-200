6e9fe0185a889d9881f61ac6bda00fa145ff0450
suncerrae <sun.quinones@bryanuniverrsity.edu>
2021-05-22 19:21:10 -0400

    x

---
+++ week2/class/class.py
@@ -0,0 +1,5 @@
+class myClass:
+    question = input('What is your favorite season, winter, spring, summer or fall? ')
+
+x = myClass()
+print(x.question.upper())
---
+++ week2/variable/variable.py
@@ -0,0 +1,9 @@
+course = 2
+name = 'John'
+print('Your son ' + name + ' is failing ' + str(course) + ' subjects.')
+print(name + ' will need to redo ' + str(course) + ' courses.')
+
+def nameFunction():
+    name = 'Eric'
+    print(name + ' is doing well in geography.')
+nameFunction()
