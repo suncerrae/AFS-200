61cf4bfc5e77707911afe8208fb6f43a81ff9df3
suncerrae <sun.quinones@bryanuniverrsity.edu>
2021-06-05 19:56:27 -0400

    x

--- week4/game.py
+++ week4/game.py
@@ -1,30 +1,36 @@
 import random
 import math
 
-lowest = int(input("Enter a number 1 here: "))
-highest = int(input("Enter a number 9 here: "))
+exit = False
+while exit is not True:
+    lowest = 1
+    highest =9
 
-x = random.randint(lowest, highest)
-print("\n\tYou've only have", 
-       round(math.log(highest - lowest + 1, 2)),
-      "chances to guess the number!\n")
+    x = random.randint(lowest, highest)
+    print("\n\tYou've only have", 
+        round(math.log(highest - lowest + 1, 2)),
+        "chances to guess the number!\n")
 
-count = 0
+    count = 0
 
-while count < math.log(highest - lowest + 1, 2):
-    count += 1
- 
-    guess = int(input("Guess a number: "))
- 
-    if x == guess:
-        print("Great job, you guessed it in the",
-              count, "try!")
-        break
-    elif x > guess:
-        print("The number you guessed is too low!")
-    elif x < guess:
-        print("The number you guessed is too high!")
- 
-if count >= math.log(highest - lowest + 1, 2):
-    print("\nThe number is %d" % x)
-    print("\t Sorry you did not get it correct! Better Luck Next time!")
+    while count < math.log(highest - lowest , 2):
+        count += 1
+        guess = (input("Guess a number: "))
+        if len (guess)<2:
+            if x == int (guess): 
+                print("Great job, you guessed it in the",
+                    count, "try!")
+                break
+            elif x > int (guess): 
+                print("The number you guessed is too low!")
+            elif x < int (guess): 
+                print("The number you guessed is too high!")
+            if count == math.log(highest - lowest , 2) and x is not int (guess):
+                print("\nThe number is %d" % x)
+                print("\t Sorry you did not get it correct! Better Luck Next time!")
+        else:
+            exit=True 
+            break
+    
+       
+print("Bye Bye")
